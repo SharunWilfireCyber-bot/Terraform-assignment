4c5a1aa0f29e501af83cb4aea677c6265feb3009
unknown <sharonharry18@gmail.com>
2026-09-14 15:40:06 +0100

    Remove credentials and add Terraform gitignore

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 terraform {
-    required_providers {
-     google = {
-      source = "hashicorp/google"
-       version = ">= 4.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0"
     }
   }
 }
 
 provider "google" {
-   project = "sharon-project-15717"
-   credentials = file("sharon-project-15717-3d096ef5e1d4.json")
+  project     = "sharon-project-15717"
 }
